4395630a1e878d010600c2bc42afecac471464b1
Simon Braines <simon.braines@gmail.com>
2015-02-25 10:24:17 +1100

    Waiting for ES to start up in start-elasticsearch

--- start-elasticsearch.sh
+++ start-elasticsearch.sh
@@ -1,1 +1,18 @@
+function waitForElastticSearchStartup
+{
+
+  for (( ; ; ))
+  do
+    curl -silent ec2-54-66-220-194.ap-southeast-2.compute.amazonaws.com:9200/_cluster/state/version >/dev/null
+
+    if [ $? -eq 0 ]; then
+      break
+    else
+      echo Waiting for ElasticSearch to start up...
+      sleep 5
+    fi
+  done
+}
+
 service elasticsearch start
+waitForElastticSearchStartup
